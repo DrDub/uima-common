c41204ecc156f753a8da37d99a6514d1cb7f3954
nicolas.hernandez <nicolas.hernandez@fa949082-bb7d-39f1-fba7-a1a5971b2ec1>
2011-02-25 08:59:45 +0000

    * src/fr/univnantes/lina/util/JavaUtilities.java     * ArrayList<String> readFromFileToLineArray(String fileName)     * ArrayList<String> readFromFileToLineArray(File file)

--- trunk/src/fr/univnantes/lina/util/JavaUtilities.java
+++ trunk/src/fr/univnantes/lina/util/JavaUtilities.java
@@ -18,10 +18,19 @@
  */
 package fr.univnantes.lina.util;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 
 /**
  * @author hernandez-n
@@ -34,15 +43,15 @@ public class JavaUtilities {
 	 * Fusionne deux tableaux de Type T en un seul
 	 * */
 	public static <T> T[] concat (T[] a, T[] b) {
-	    final int alen = a.length;
-	    final int blen = b.length;
-	    final T[] result = (T[]) java.lang.reflect.Array.
-	            newInstance(a.getClass().getComponentType(), alen + blen);
-	    System.arraycopy(a, 0, result, 0, alen);
-	    System.arraycopy(b, 0, result, alen, blen);
-	    return result;
+		final int alen = a.length;
+		final int blen = b.length;
+		final T[] result = (T[]) java.lang.reflect.Array.
+		newInstance(a.getClass().getComponentType(), alen + blen);
+		System.arraycopy(a, 0, result, 0, alen);
+		System.arraycopy(b, 0, result, alen, blen);
+		return result;
 	}
-	
+
 	/**
 	 * Create a temporary text file with a text given in parameter and return its absolute path
 	 * @param prefixTmpFile
@@ -62,4 +71,114 @@ public class JavaUtilities {
 		out.close();
 		return fileAbsPathStrg;
 	}
+
+
+	/**
+	 * Utility method to write a given text to a file
+	 * fileName doit intégrer le nom du répertoire +"/"
+	 */
+	public static boolean writeLineArrayToFile(String fileName, String[] lines  ) {
+
+		Writer out = null;
+		//System.out.println("Debug: full Output File Name "+ fileName);
+
+		try {
+
+			out = new OutputStreamWriter(new FileOutputStream(fileName),"UTF8");
+
+			for (int l = 0 ;  l< lines.length ; l++) {
+				//System.out.println("Debug: current line "+ lines[l]);
+
+				out.write(lines[l]);
+			}
+			out.close();
+		} catch (FileNotFoundException ex) {
+			return (false);
+		} catch (IOException ex) {
+			return (false);
+		}
+		return (true);
+
+	}
+
+
+	/**
+	 * Utility method to read a given text from a file
+	 * fileName doit intégrer le nom du répertoire dans le filename"/"
+	 * @throws IOException 
+	 */
+	public static ArrayList<String> readFromFileToLineArray(String fileName){
+		System.out.println("Debug: full input File Name "+ fileName);
+		ArrayList<String> lines = new ArrayList<String>();
+
+		// Open the file that is the first 
+		// command line parameter
+		FileInputStream fstream = null;
+		try {
+			fstream = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine; 
+		//Read File Line By Line
+		try {
+			while ((strLine = br.readLine()) != null)   {
+				lines.add(strLine);
+				// Print the content on the console
+				//System.out.println (strLine);
+			}
+			//Close the input stream
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return lines;
+	}
+	
+
+	/**
+	 * Utility method to read a given text from a file
+	 * fileName doit intégrer le nom du répertoire dans le filename"/"
+	 * @throws IOException 
+	 */
+	public static ArrayList<String> readFromFileToLineArray(File file){
+		System.out.println("Debug: full input File Name "+ file);
+		ArrayList<String> lines = new ArrayList<String>();
+
+		// Open the file that is the first 
+		// command line parameter
+		FileInputStream fstream = null;
+		try {
+			fstream = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine; 
+		//Read File Line By Line
+		try {
+			while ((strLine = br.readLine()) != null)   {
+				lines.add(strLine);
+				// Print the content on the console
+				//System.out.println (strLine);
+			}
+			//Close the input stream
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return lines;
+	}
+
 }
